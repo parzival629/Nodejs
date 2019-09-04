850bd36348faf0541bde8f726514c21b16f42b1f
feiber <famartinez629@gmail.com>
2019-09-04 18:53:07 -0500

    Creación de formulario de registro y Password para poder registrar producto

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 var express = require('express'); //almacenando la funcionalidad del expres
 var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
 
 var app = express();              //Donde se ejecutan los metodos
 
 mongoose.connect("mongodb://localhost:27017/primera_pagina", {userNewUrlParser: true});
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
 
 //definir shema de nuestros productos
 var productShema = {
@@ -21,7 +26,7 @@ app.use(express.static("public"))
 
 app.get("/",function(solicitud,respuesta){    //donde se reciben las peticiones url(/) PURO Dominio
   
-	var data = {
+	/*var data = {
 		tittle: "Mi primer super producto",
 		description: "super compra",
 		precio: 10
@@ -31,13 +36,35 @@ app.get("/",function(solicitud,respuesta){    //donde se reciben las peticiones 
 
 	product.save(function(err){
 		console.log(product);
-	});	
+	});*/
 
   respuesta.render("index");        
 });
 
+app.post("/menu",function(solicitud,respuesta){
+	if(solicitud.body.password =="12345678"){
+		var data = {
+			tittle: solicitud.body.tittle,
+			description: solicitud.body.description,
+			imageUrl: "data-png",
+			precio: solicitud.body.precio
+		}
+
+		var product = new Product(data);
+
+		product.save(function(err){
+			console.log(product);
+			respuesta.render("index");
+		});	
+	}else{
+		respuesta.render("menu/new")
+	}
+
+
+});
+
 app.get("/menu/new",function(solicitud,respuesta){
-	respuesta.render("menu/new")
+	respuesta.render("menu/new");
 })
 
 app.listen(8080,function(){
